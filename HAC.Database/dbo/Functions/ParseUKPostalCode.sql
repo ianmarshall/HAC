86e41e71034ea31e0e30f5459342552c042b3520
Ian Marshall <ian.marshall@btinternet.com>
2013-06-17 19:39:06 +0100

    latest

---
+++ HAC.Database/dbo/Functions/ParseUKPostalCode.sql
@@ -0,0 +1,7 @@
+﻿
+CREATE function dbo.ParseUKPostalCode(
+    @pPostalCode varchar(8)
+)
+returns table
+as return (select rtrim(left(@pPostalCode,len(@pPostalCode) - 3)) as Outward, right(@pPostalCode,3) as Inward);
+
